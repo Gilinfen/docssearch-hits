fafb073e5958a453cd2c21c95428520fd86a9d21
Glinfen <gilinfen@gmail.com>
2023-05-23 18:58:18 +0800

    perf: ⚡️ 格式化代码

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 import * as keys from './changelog.config'
 
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
     //   TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    'type-enum': [2, 'always', keys.list]
-  }
+        'type-enum': [2, 'always', keys.list],
+    },
 }
